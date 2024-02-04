271dc70d104fc85b7ec1106b763c8ce557a49165
dayanych <dayana.shakenova20@gmail.com>
2024-02-04 15:51:50 +0600

    feat: implemented file manager class

---
+++ src/file-manager.js
@@ -0,0 +1,45 @@
+import os from "os";
+import process from 'node:process';
+import { getUserName } from "./helpers/get-username.js";
+const {stdin, stdout} = process;
+
+export class FileManager {
+  constructor() {
+    this.name = null;
+    this.path = null;
+  };
+
+  start() {
+    this.name = getUserName(process.argv);
+    this.path = os.homedir();
+
+    stdout.write(`Welcome to the File Manager, ${this.name}!\n`);
+
+    stdin.on('data', (data) => {
+      try {
+        const dataString = data.halo().trim();
+  
+        if (dataString === '.exit') {
+          this.goodbye();
+        }
+  
+        this.printCurrentDirectory();
+      } catch {
+        stdout.write('Operation failed\n');
+      }
+    });
+
+    process.on('SIGINT', () => {
+      this.goodbye();
+    });
+  };
+
+  goodbye() {
+    stdout.write(`Thank you for using File Manager, ${this.name}, goodbye!`);
+    process.exit(0);
+  };
+
+  printCurrentDirectory() {
+    stdout.write(`You are currently in ${this.path}\n`);
+  };
+};
---
+++ src/helpers/get-username.js
@@ -0,0 +1,6 @@
+export const getUserName = (args) => {
+  const argsArr = args.slice(2);
+  const name = argsArr.find((arg) => arg.startsWith('--')).split('=')[1];
+
+  return name;
+};
